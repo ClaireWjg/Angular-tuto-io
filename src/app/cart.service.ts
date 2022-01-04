ac677791b10e7976fc920f393289f6c03e12f0d1
ClaireWjg <claire.wolljung@viacesi.fr>
2022-01-04 16:18:35 +0100

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CartService {
   items: Product[] = [];
-  constructor(private http: HttpCleint) {}
+  constructor(private http: HttpClient) {}
 
   addToCart(product: Product) {
     this.items.push(product);
